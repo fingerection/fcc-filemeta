613069e80574e75856f1480760f44fc2207c1bf2
fingerection <fingerection@gmail.com>
2016-06-11 09:43:40 +0000

    finish file meta

--- server.js
+++ server.js
@@ -1,33 +1,18 @@
-'use strict';
-
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
-
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
+var path = require('path');
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' })
 
-mongoose.connect(process.env.MONGO_URI);
+app.get('/', function(req,res){
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
+app.post('/api/fileanalyse/', upload.single('0'), function(req, res){
+	res.send({fileSize:req.file.size});
+});
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+  console.log('Example app listening on port'+port);
 });
