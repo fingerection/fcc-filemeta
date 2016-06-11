d004214e4e9ea2bec5d0069a38e5eea4800641d2
fingerection <fingerection@gmail.com>
2016-06-11 09:57:27 +0000

    fit heroku path

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ var app = express();
 var multer = require('multer');
 var upload = multer({ dest: 'uploads/' })
 
+// At the top of your web.js
+process.env.PWD = process.cwd()
+
 app.get('/', function(req,res){
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(process.env.PWD + '/index.html');
 });
 
 app.post('/api/fileanalyse/', upload.single('0'), function(req, res){
